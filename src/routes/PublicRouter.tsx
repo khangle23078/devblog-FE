ee3ad5e4c28109c11717c09d5302a8aacf6e8049
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2023-10-12 05:35:26 +0700

    create admin layout

--- src/routes/PublicRouter.tsx
+++ src/routes/PublicRouter.tsx
@@ -2,11 +2,24 @@
 import {createBrowserRouter} from "react-router-dom";
 import {lazy} from "react";
 
+const AdminLayout = lazy(() => import("./../layouts/AdminLayout"));
+
 const Login = lazy(() => import("./../pages/auth/Login"));
+const Dashboard = lazy(() => import("./../pages/admin/Dashboard"));
 
 export const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "admin",
+    element: <AdminLayout />,
+    children: [
+      {
+        path: "",
+        element: <Dashboard />,
+      },
+    ],
   },
 ]);
